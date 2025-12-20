7dca84e7fd2d030e0bc8429fd396adb3448bf38c
Uttej <storiesbyuttej@gmail.com>
2025-12-20 05:53:25 +0000

    Day 2: Variables and dynamic output for automation

---
+++ python/day2_variables_and_output.py
@@ -0,0 +1,8 @@
+username = "admin"
+password = "admin123"
+environment = "staging"
+
+print("Running automation test")
+print(f"Username: {username}")
+print(f"Passwrod: {password}")
+print(f"Environment: {environment}")
