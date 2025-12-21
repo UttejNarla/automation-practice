d0b39144f8f8e568450f00975f59e3431d96fdce
Uttej <storiesbyuttej@gmail.com>
2025-12-21 05:37:19 +0000

    Practice 7 updated

--- python/practice/practice7.py
+++ python/practice/practice7.py
@@ -1,6 +1,7 @@
 test = "checkout"
+failed_test = ["checkout"]
 
-if test == "checkout":
+if test in failed_test:
 	print(f"{test} FAILED")
 	for attempt in range (1,4):
 		print(f"Retry {attempt} for {test}")
